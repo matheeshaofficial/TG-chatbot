917267ce2ee54abb6b258e25ae2ac0abddda0549
youtubeslgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-06-28 19:47:24 -0700

    Update start.py

--- start.py
+++ start.py
@@ -3,13 +3,13 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 LYCIA_START = """
-Hello, I am Lycia [リュキア], An Intelligent ChatBot. If You Are Feeling Lonely, You can Always Come to me and Chat With Me!
+😃 Hello, I am TG-chatbot , An Intelligent ChatBot. You can Always Come to me and Chat With Me! join with us - @slbotzone 
 """
 
 
 @LYCIA.on_message(filters.command(["start"], prefixes = "/") & ~filters.edited)
 async def info(client, message):
     buttons = [
-                [InlineKeyboardButton("Lycia", switch_inline_query_current_chat="lycia "), InlineKeyboardButton("Github", url = "https://github.com/Red-Aura/Lyciachatbot")]
+                [InlineKeyboardButton("🔥TG- chatbot 🔥", switch_inline_query_current_chat="🔥TG- chatbot 🔥 "), InlineKeyboardButton(" 📦 my socure code 📦", url = "https://github.com/youtubeslgeekshow/TG-chatbot")]
               ]
     await LYCIA.send_message(chat_id = message.chat.id, text = LYCIA_START, reply_markup = InlineKeyboardMarkup(buttons))
